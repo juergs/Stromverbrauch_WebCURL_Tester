62c7625235959618694d5597e7bf7f518469d497
Juergen <juergen@jschweiss.de>
2018-04-09 20:49:36 +0200

    Added
    Test Counter by polling counter-Url

---
+++ WebCURL/WebCURL/frmMain.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class WebCURL
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtCurl = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.sbar = New System.Windows.Forms.StatusStrip()
+        Me.sLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.sLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtIP = New System.Windows.Forms.MaskedTextBox()
+        Me.btnIPEnter = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.sbar.SuspendLayout
+        Me.GroupBox1.SuspendLayout
+        Me.SuspendLayout
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(221, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Run"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'txtCurl
+        '
+        Me.txtCurl.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.txtCurl.BackColor = System.Drawing.SystemColors.Info
+        Me.txtCurl.Location = New System.Drawing.Point(0, -1)
+        Me.txtCurl.Multiline = true
+        Me.txtCurl.Name = "txtCurl"
+        Me.txtCurl.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtCurl.Size = New System.Drawing.Size(377, 232)
+        Me.txtCurl.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60000
+        '
+        'sbar
+        '
+        Me.sbar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.sLabel, Me.sLabel2, Me.ToolStripStatusLabel3})
+        Me.sbar.Location = New System.Drawing.Point(0, 288)
+        Me.sbar.Name = "sbar"
+        Me.sbar.Size = New System.Drawing.Size(377, 22)
+        Me.sbar.TabIndex = 2
+        '
+        'sLabel
+        '
+        Me.sLabel.Name = "sLabel"
+        Me.sLabel.Size = New System.Drawing.Size(312, 17)
+        Me.sLabel.Spring = true
+        Me.sLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'sLabel2
+        '
+        Me.sLabel2.AutoSize = false
+        Me.sLabel2.Name = "sLabel2"
+        Me.sLabel2.Size = New System.Drawing.Size(50, 17)
+        '
+        'ToolStripStatusLabel3
+        '
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(0, 17)
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(299, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 20)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "&End"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'txtIP
+        '
+        Me.txtIP.Location = New System.Drawing.Point(13, 20)
+        Me.txtIP.Name = "txtIP"
+        Me.txtIP.Size = New System.Drawing.Size(106, 20)
+        Me.txtIP.TabIndex = 4
+        '
+        'btnIPEnter
+        '
+        Me.btnIPEnter.Font = New System.Drawing.Font("Webdings", 12!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2,Byte))
+        Me.btnIPEnter.ForeColor = System.Drawing.Color.CornflowerBlue
+        Me.btnIPEnter.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btnIPEnter.Location = New System.Drawing.Point(125, 18)
+        Me.btnIPEnter.Name = "btnIPEnter"
+        Me.btnIPEnter.Size = New System.Drawing.Size(24, 21)
+        Me.btnIPEnter.TabIndex = 5
+        Me.btnIPEnter.Text = "a"
+        Me.btnIPEnter.UseVisualStyleBackColor = true
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left)  _
+            Or System.Windows.Forms.AnchorStyles.Right),System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.btnIPEnter)
+        Me.GroupBox1.Controls.Add(Me.txtIP)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 232)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(377, 56)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = false
+        '
+        'WebCURL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(377, 310)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.sbar)
+        Me.Controls.Add(Me.txtCurl)
+        Me.Name = "WebCURL"
+        Me.Text = "WebCurl"
+        Me.sbar.ResumeLayout(false)
+        Me.sbar.PerformLayout
+        Me.GroupBox1.ResumeLayout(false)
+        Me.GroupBox1.PerformLayout
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtCurl As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents sbar As StatusStrip
+    Friend WithEvents sLabel As ToolStripStatusLabel
+    Friend WithEvents sLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel3 As ToolStripStatusLabel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtIP As MaskedTextBox
+    Friend WithEvents btnIPEnter As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ WebCURL/WebCURL/frmMain.vb
@@ -0,0 +1,81 @@
+﻿Imports System.String 
+Imports Microsoft.VisualBasic
+
+Public Class WebCURL
+    Private _webClient As System.Net.WebClient
+    Private strIpAddress As String = "192.168.0.1"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        sLabel.Text = "Waiting to enable."
+        _webClient = new System.Net.WebClient
+        txtIP.text = strIpAddress
+    End Sub
+
+    Public Function GetCurlData() as string             
+        Try
+            Return  _webClient.DownloadString("http://" & strIpAddress)
+        Catch ex as System.Net.WebException
+            Return  "... Error:" & ex.InnerException.ToString
+        End try
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = not Timer1.Enabled
+        sLabel.Text = IIf(Timer1.Enabled, "Enabled","Disabled")
+        sLabel2.Text = "Started"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim strData = GetCurlData
+        txtCurl.Text = txtCurl.Text & getTimestamp & vbTab & strData & Environment.NewLine
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit
+    End Sub
+
+    Private function getTimestamp()  As String 
+        Dim myTime As DateTime = DateTime.Now
+        Dim strTimeStamp = Microsoft.VisualBasic.Right ("00" & myTime.Hour, 2 ) & ":" & _
+            Microsoft.VisualBasic.Right ("00" & myTime.Minute, 2) & ":" & _
+            Microsoft.VisualBasic.Right ("00" & myTime.Second, 2) & "." & _
+            Microsoft.VisualBasic.Right( "000" & myTime.Millisecond,3) 
+
+        Return strTimeStamp
+    End function
+
+
+     Private Sub txtIP_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtIP.KeyPress
+        If Char.IsDigit(e.KeyChar) Or e.KeyChar = "." Or Asc(e.KeyChar) = Keys.Delete Or _
+           Asc(e.KeyChar) = Keys.Right Or Asc(e.KeyChar) = Keys.Left Or Asc(e.KeyChar) = Keys.Delete Or Asc(e.KeyChar) = Keys.Back Then
+            Return
+        End If
+        e.Handled = True
+    End Sub
+
+    Private Sub txtIP_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtIP.LostFocus
+        Dim IP() As String = txtIP.Text.Split(".")
+        Dim Test As Integer
+        If IP.Length = 4 Then 'If 3 "."
+            Dim Proper As Boolean = True
+            For I As Integer = 0 To 3
+                Test = Integer.Parse(IP(I)) 'Parse the string for an integer, if its not return -1
+                If Test < 0 Or Test > 255 Then 'If not between 0-255 then the ip is not a proper format
+                    MsgBox("IP Address is not in a proper format! All numbers entered must be between 0 - 255")
+                    txtIP.Focus()
+                    txtIP.SelectionStart = txtIP.Text.Length
+                    Return
+                End If
+            Next
+        Else
+            MsgBox("IP Address is not in a proper format! There must be 4 numbers entered, in XXX.XXX.XXX.XXX format")
+            txtIP.Focus()
+            txtIP.SelectionStart = txtIP.Text.Length
+        End If
+    End Sub
+
+    Private Sub btnIPEnter_Click(sender As Object, e As EventArgs) Handles btnIPEnter.Click
+        strIpAddress = txtIP.Text
+    End Sub
+End Class
